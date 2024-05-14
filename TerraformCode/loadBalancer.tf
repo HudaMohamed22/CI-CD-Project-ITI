5545207914ce87d1624ab2917a73fc77ba4f0128
Huda22 <hudamohamed2223@gmail.com>
2024-05-14 20:00:50 +0300

    fix bugs

--- TerraformCode/loadBalancer.tf
+++ TerraformCode/loadBalancer.tf
@@ -16,13 +16,34 @@ resource "aws_lb_target_group" "alb_target_group" {
   }
 
 }
+# Define a security group for alb
+resource "aws_security_group" "alb_sg" {
+  vpc_id   = module.network.M-vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
 
 # Create an ALB
 resource "aws_lb" "node_alb" {
   name               = "NodeJs-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.ssh_from_anywhere.id]
   subnets            = [ module.network.M-subnets["public"].id,  module.network.M-subnets["public2"].id]
 
   tags = {
@@ -42,10 +63,3 @@ resource "aws_lb_listener" "alb_listener" {
   }
 }
 
-
-# Attach instances to target group
-resource "aws_lb_target_group_attachment" "alb_target_group_attachment" {
-  target_group_arn  = aws_lb_target_group.alb_target_group.arn
-  target_id         = aws_instance.application.id
-  port              = 3000
-}
